8562987978124effb5a8a68e20ded2be820ceccb
simo1912-dev <127667529+simo1912-dev@users.noreply.github.com>
2025-07-31 16:18:12 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ def send_email(message, user_email):
     host = "smtp.gmail.com"
     port = 465
     username = "simoaboulfath52@gmail.com"
-    password = "neeefyjespjuhkcb"
+    password = ""
     receiver = "simoaboulfath52@gmail.com"
     context = ssl.create_default_context()
     email_message = f"Subject: New Contact Message\n\n{message}\n\nFrom: {user_email}"
